fedf657ef3316d6cf3388d0f391e208f2cfdaa35
nguythang <thangnvse04385@fpt.edu.vn>
2019-10-14 01:32:16 +0700

    First

---
+++ config.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+  url: 'https://lumen.netlify.com',
+  pathPrefix: '/github bboy114crew',
+  title: 'Blog by nguythang',
+  subtitle: 'My world',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: 'UA-73379983-2',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+    {
+      label: 'Contact me',
+      path: '/pages/contacts'
+    }
+  ],
+  author: {
+    name: 'Nguy Thang',
+    photo: '/photo.jpg',
+    bio: 'SE, Lover',
+    contacts: {
+      email: '',
+      facebook: '#',
+      telegram: '#',
+      twitter: '#',
+      github: '#',
+      rss: '',
+      vkontakte: '',
+      linkedin: '#',
+      instagram: '#',
+      line: '',
+      gitlab: '',
+      weibo: ''
+    }
+  }
+};
