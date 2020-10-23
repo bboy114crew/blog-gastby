9b39bf9a8e85f60d0b6fae599d382deb83069a38
Thắng Nguỵ <bboy114crew@gmail.com>
2020-10-23 10:00:17 +0700

    [f] Update Not found page

--- src/templates/not-found-template.js
+++ src/templates/not-found-template.js
@@ -1,21 +1,71 @@
 // @flow strict
 import React from 'react';
+import { graphql } from 'gatsby';
 import Sidebar from '../components/Sidebar';
 import Layout from '../components/Layout';
 import Page from '../components/Page';
 import { useSiteMetadata } from '../hooks';
 
-const NotFoundTemplate = () => {
+const NotFoundTemplate = ({ data }) => {
   const { title, subtitle } = useSiteMetadata();
-
   return (
     <Layout title={`Not Found - ${title}`} description={subtitle}>
       <Sidebar />
-      <Page title="NOT FOUND">
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <Page>
+        <div style={{
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center'
+        }}>
+          <p
+            style={{
+              textAlign: 'center',
+              color: 'rgba(0, 0, 0, 0.85)',
+              fontSize: 24,
+              lineHeight: 1.8,
+              marginBottom: 0,
+            }}
+          >
+            404
+          </p>
+          <p
+            style={{
+              textAlign: 'center',
+              color: 'rgba(0,0,0,.45)',
+              fontSize: 14,
+              lineHeight: 1.6,
+              marginBottom: 0,
+            }}
+          >
+            Sorry, the page you visited does not exist.
+          </p>
+          <img
+            src={data.allImageSharp.nodes[0].original.src}
+            alt="Not found image"
+            style={{
+              maxHeight: 400,
+            }}
+          />
+        </div>
       </Page>
     </Layout>
   );
 };
+
+export const query = graphql`
+  {
+    allImageSharp(
+      filter: { id: { eq: "c173548b-71e4-558e-b627-7508ee7fbead" } }
+    ) {
+      nodes {
+        id
+        original {
+          src
+        }
+      }
+    }
+  }
+`;
 
 export default NotFoundTemplate;
